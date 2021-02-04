e890d56ab0aee533b82a5e04bec4b3414168391e
Nicol Arias <nicolariaslebro@gmail.com>
2021-02-04 21:47:47 +0100

    Punto 7 de los ejercicios de la unidad 5.

---
+++ UD5Punto7/src/punto7/Punto7App.java
@@ -0,0 +1,18 @@
+package punto7;
+/*FECHA: 03/02/2021
+ * Nicol Dayana Arias Lebro*/
+public class Punto7App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*7) Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+		 */
+		int num=1;
+		while(num<=100) {
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
