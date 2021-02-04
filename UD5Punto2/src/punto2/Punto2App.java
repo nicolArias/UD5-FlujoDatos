18d3ca676746119e93ca4643774ef1e832c0d2e9
Nicol Arias <nicolariaslebro@gmail.com>
2021-02-04 21:34:51 +0100

    Punto 2 de los ejercicios de la unidad 5.

---
+++ UD5Punto2/src/punto2/Punto2App.java
@@ -0,0 +1,17 @@
+package punto2;
+/*FECHA: 03/02/2021
+ * Nicol Dayana Arias Lebro*/
+public class Punto2App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*2) Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
+			por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”*/
+		
+		String nombre="Nicol Arias";
+		
+		System.out.println("\"Bienvenida "+nombre+"\" ");
+
+	}
+
+}
