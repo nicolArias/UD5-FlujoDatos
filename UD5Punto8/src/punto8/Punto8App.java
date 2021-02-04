82bc70718e6fd098b257bf2f1e787f14fe274506
Nicol Arias <nicolariaslebro@gmail.com>
2021-02-04 21:48:37 +0100

    Punto 8 de los ejercicios de la unidad 5.

---
+++ UD5Punto8/src/punto8/Punto8App.java
@@ -0,0 +1,15 @@
+package punto8;
+/*FECHA: 03/02/2021
+ * Nicol Dayana Arias Lebro*/
+public class Punto8App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*8) Haz el mismo ejercicio anterior con un bucle for.*/
+
+		for(int num=1;num<=100;num++) {
+			System.out.println(num);
+		}
+	}
+
+}
